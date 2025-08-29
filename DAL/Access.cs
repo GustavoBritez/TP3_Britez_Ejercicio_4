7f6f378933aca3087504ce8a04c130e77729111a
Mecha <danielgus@outlook.es>
2025-08-29 20:40:56 -0300

    Abrimos la conexion 2

--- DAL/Access.cs
+++ DAL/Access.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    internal class Access
+    public class Access
     {
+        private SqlConnection conexion = new SqlConnection();
+        private SqlTransaction transaction;
+
+        public void Open()
+        {
+            conexion.Open();
+        }
+        public void Close()
+        {
+            conexion.Close();
+        }
+        public void Start_TX()
+        {
+            transaction = conexion.BeginTransaction();
+        }
+
+        public void Cancel_TX()
+        {
+            transaction.Rollback();
+        }
+        public void Close_TX()
+        {
+            transaction.Commit();
+        }
     }
 }
