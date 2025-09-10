95ae6e9a005db4efa1ab99b5facc69939b3f3c38
Gus Britez <danielgus@outlook.es>
2025-09-09 22:02:19 -0300

    creacion de prestamo clase BE

---
+++ BE/PrestamoBE.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class PrestamoBE
+    {
+        private string autor;
+        private string titulo;
+        private int ejemplar;
+        private string nombre;
+        private string mail;
+        private DateTime fecha_Retiro;
+        private DateTime fecha_Devuelta;
+
+        public string Autor { get { return autor; } set { autor = value; } }
+        public string Titulo { get { return titulo; } set { titulo = value; } }
+        public int Ejemplar { get { return ejemplar; } set { ejemplar = value; } }
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string Mail { get { return mail; } set { mail = value; } }
+        public DateTime Fecha_Retiro {  get { return fecha_Retiro; } set { fecha_Retiro = value; } }
+        public DateTime Fecha_Devuelta {  get { return fecha_Devuelta; } set { fecha_Devuelta = value; } }
+    }
+}
---
+++ BLL/PrestamoBLL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class PrestamoBLL
+    {
+        public void Solicitar_Prestamo()
+        {
+
+        }
+        public void Devolver_Prestamo()
+        {
+
+        }
+        public void Obtener_Prestamos()
+        {
+
+        }
+    }
+}
